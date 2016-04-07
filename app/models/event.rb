3ff306da23c04ba89ebb175d9710972128a69a93
Mateo Mejia <mateomgj@gmail.com>
2016-04-06 19:09:20 -0500

    All associations working

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  belongs_to :creator, class_name: "User"
+  belongs_to :creator, class_name: "User", foreign_key: :user_id
   
   has_many :goings, foreign_key: :event_id
   has_many :atendees, through: :goings, source: :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-	has_many :events, foreign_key: :user_id
+	has_many :events
 
 	has_many :goings, foreign_key: :user_id  #:atendee_id #Does he add both foreign keys to the model?
-	has_many :attended_events, through: :goings
+	has_many :attended_events, through: :goings, source: :event
 end
